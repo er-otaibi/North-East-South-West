2b62ad20c4c9d100abb4a540775e4d02ccec1140
Rehab B <79226830+er-otaibi@users.noreply.github.com>
2021-12-12 13:23:02 +0300

    Initial Commit

---
+++ North East South West/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  North East South West
+//
+//  Created by Mac on 08/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
